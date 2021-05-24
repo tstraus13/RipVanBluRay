2bcfa700d7c9cb0d3222a75143a40c8291d42006
Tom Strausbaugh <tstrausbaugh@straustech.net>
2021-05-24 16:26:47 -0400

    Minor fix

--- RipVanMonitor/Startup.cs
+++ RipVanMonitor/Startup.cs
@@ -29,6 +29,7 @@ namespace RipVanMonitor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<RipService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
